a2672b30d2bfba28d2f64ed3027fb73c381cc128
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-05-14 23:25:30 +1200

    Update TEST_EXTERNAL_REFERENCE URL

--- libcellml_tutorials.cmake
+++ libcellml_tutorials.cmake
@@ -1,1 +1,1 @@
-set(TEST_EXTERNAL_REFERENCE "https://raw.githubusercontent.com/libcellml/end-to-end-test-database/86ad099bec0f5a5fb77c2b3331492ec53fb904bd/libcellml_tutorials.cmake")
+set(TEST_EXTERNAL_REFERENCE "https://raw.githubusercontent.com/libcellml/end-to-end-test-database/3195d58210aafa1eace2a57087e896438f5ee12e/libcellml_tutorials.cmake")
